206062c084db685818fa3d20fc51282c68d9b4ea
Achernogolov <chernogolovka@list.ru>
2020-04-26 17:34:39 +0000

    Добавлен роут очистки кэша

---
+++ src/Routes/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+Route::get('/clear', function() {
+    Artisan::call('cache:clear');
+    Artisan::call('config:cache');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    return "Cache clear";
+});
